d33d73622c9408336d5f88803bcb12aed3117ee7
Níkolas <nikolas.oliveira0701@gmail.com>
2023-03-09 00:47:30 -0300

    project

---
+++ README.md
@@ -0,0 +1,10 @@
+Fatec Diadema
+Desenvolvimento de Software Multiplataforma - 3º Semestre
+Matéria: Tecnicas de Programação II
+Professor: Vinicius Heltai
+
+Alunos: Daniel Irineu
+        Níkolas Yan de Oliveira Costa
+        Víctor dos Santos Viana
+
+Resumo do projeto: O sistema sauda o usuário, e solicita seu nome. Após isso, solicita que o usuário indique qual é a função dele na instituição, representada por um código que deve ser digitado por ele. Em seguida, o sistema irá retornar o nome do usuário, mais a sua função. Caso o usuário insira um código que não esteja de acordo com nenhuma função na instituição, ele exibirá uma mensagem "opção inválida". O sistema continua rodando quantas vezes necessário até que o usuário digite a letra S, para encerrar o sistema.
---
+++ main.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractclassmethod
+
+# classe abstrata que define um metodo abstrato a ser implementado
+class Pessoa(ABC):
+    @abstractclassmethod
+    def relacao_com_fatec(self):
+        pass
+
+# subclasse de Pessoa
+class Aluno(Pessoa):
+    def relacao_com_fatec(self):
+        return "Aluno"
+
+# subclasse de Pessoa
+class Professor(Pessoa):
+    def relacao_com_fatec(self):
+        return "Professor"
+
+# subclasse de Pessoa
+class Coordenador(Pessoa):
+    def relacao_com_fatec(self):
+        return "Coordenador"
+
+# subclasse de Pessoa
+class Diretor(Pessoa):
+    def relacao_com_fatec(self):
+        return "Diretor"
+
+# subclasse de Pessoa
+class Administrativo(Pessoa):
+    def relacao_com_fatec(self):
+        return "Administrativo"
+
+# subclasse de Pessoa
+class Vestibulando(Pessoa):
+    def relacao_com_fatec(self):
+        return "Vestibulando"
+
+# classe Factory Method
+class PessoaFactory:
+ 
+    
+    def criar_pessoa(relacao):
+        if relacao == "Aluno":
+            return Aluno()
+        elif relacao == "Professor":
+            return Professor()
+        elif relacao == "Coordenador":
+            return Coordenador()
+        elif relacao == "Diretor":
+            return Diretor()
+        elif relacao == "Administrativo":
+            return Administrativo()
+        elif relacao == "Vestibulando":
+            return Vestibulando()
+        else:
+            return None
+          
+    def menu():
+        print('\n*------------------------*')
+        print ('| 1 -- Aluno             |' )
+        print ('| 2 -- Professor         |' )
+        print ('| 3 -- Coordenador       |' )
+        print ('| 4 -- Diretor           |' )
+        print ('| 5 -- Administrativo    |' )
+        print ('| 6 -- Vestibulando      |' )
+        print('*------------------------*')
+  
+        option = input('\nEscolha sua relação com a FATEC: ')
+      
+        if option == "1":
+            relacao = "Aluno"
+        elif option == "2":
+            relacao = "Professor"
+        elif option == "3":
+            relacao = "Coordenador"
+        elif option == "4":
+            relacao = "Diretor"
+        elif option == "5":
+            relacao = "Administrativo"
+        elif option == "6":
+            relacao = "Vestibulando"
+        else:
+            print("\n\n Opção invalida!")
+            return None
+        return relacao
+
+sair = "C"
+while(sair != "S"):
+  nome = input("Bem-vindo ao sistema! Digite o seu nome: ")
+  relacao = PessoaFactory.menu()
+  pessoa = PessoaFactory.criar_pessoa(relacao)
+  
+  if pessoa:
+      print("\n\nOlá, " + nome + "! você tem relação com a instituição como " + pessoa.relacao_com_fatec())
+  else:
+      print("\n\n" + nome + " não tem nenhuma relação com a instituição, acompanhar até a secretaria")
+  sair = input("\nDigite S para sair, ou qualquer outro caractere para continuar.")
